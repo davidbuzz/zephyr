c36a99db592223834bbd2f01c692f6524be3a54a
David 'Buzz' Bussenschutt <d.bussenschutt@uq.edu.au>
2024-03-23 13:46:47 +1000

    wip

--- boards/common/esp32.board.cmake
+++ boards/common/esp32.board.cmake
@@ -4,6 +4,7 @@ board_set_flasher_ifnset(esp32)
 board_set_debugger_ifnset(openocd)
 
 board_runner_args(openocd  --no-init --no-halt --no-targets --no-load)
+board_runner_args(openocd  --gdb-init "appimage_offset 0x10000")
 board_runner_args(openocd  --gdb-init "set remote hardware-watchpoint-limit 2")
 
 board_runner_args(openocd  --gdb-init "flushregs")
